3da57c6376cf17c42d2e5328051577a9177e23ab
Lucas Freitas <lfreitas@college.harvard.edu>
2014-02-19 20:20:17 -0500

    added files

---
+++ assets/js/scripts.js
@@ -0,0 +1,27 @@
+var photo = Math.round(Math.random() * 8);
+var name = "assets/img/" + photo + ".jpg";
+document.getElementById("top").style.background = "url('" + name + "')";
+
+document.createElement('li');
+document.createElement('figcaption');
+
+$(window).load(function(){
+    $(document).ready(function(){
+
+        $(document).scroll(function() {
+
+            var top = $(document).scrollTop();
+
+            if (top > 542) {
+                $('#wrapper-about').css({'position': 'fixed', 'top': '0', 'background-color': 'white'});
+                $('#wrapper-projects').css({'position': 'relative', 'top': '510px'});
+          	}
+          	else {
+          		$('#wrapper-about').css({'position': 'relative'});
+          		$('#wrapper-projects').css({'position': 'relative', 'top': '0px'});
+          	}
+          	
+          	return false;
+        });
+    });
+});
---
+++ index.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+	<head>
+		<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
+		<title>Brandon Smith</title>
+		<link rel='stylesheet' media="screen" type='text/css' href='assets/css/styles.css'/>
+		<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	</head>
+
+	<body>
+		<div class="top" id="top">
+		</div>
+		<div class="top-text">
+			<h1>Brandon Smith</h1>
+			<h2 class="long">Software Engineer, Ohio State Student, and Design Enthusiast</h2>
+			<h2 class="short">Software Engineer</h2>
+		</div>
+		<div class="body">
+			<div class="content">
+				<div id="wrapper-about">
+					<div class="about">
+						<h3>- ABOUT ME -</h3>
+						<hr></hr>
+						<div class="left">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tempor suscipit justo, id ultricies ipsum sodales non. Duis dictum fringilla iaculis. Cras elementum convallis neque in interdum. Aenean quis dui hendrerit, semper nibh sed, pretium orci. Nunc fermentum luctus mi a sollicitudin. Nam in ante a velit venenatis auctor. Duis semper dolor orci, et sagittis quam posuere vel. Aliquam lacinia, risus sed tincidunt lobortis, risus ipsum ultrices augue, at fringilla eros lorem in dolor. Suspendisse nec risus nisl. Mauris vitae adipiscing massa. </p>
+							<p>Nulla vitae eros at elit gravida dictum in ut purus. Etiam rutrum erat vitae scelerisque semper. Aenean urna elit, congue in interdum nec, lobortis nec ligula. Vivamus turpis eros, rutrum nec urna non, vulputate egestas dui. Quisque varius massa non tristique bibendum. Nunc lacinia aliquam consequat. Vestibulum a sagittis neque, ut porttitor massa.</p>
+						</div>
+						<div class="right">
+							<div class="img">
+								<img src="assets/img/glitchr.jpg"></img>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="projects" id="wrapper-projects">
+					<h3>- PROJECTS -</h3>
+					<hr></hr>
+					<ul class="projects">
+						<a class="project-link" href="https://play.google.com/store/apps/details?id=com.apporio.glitchr" target="_blank">
+							<li><img src="assets/img/glitchr.jpg"></img>
+								<figcaption>
+									<h4>Glitchr</h4>
+									<p>Glitchr is an Android app that lets you give your photos a cool retro look!</p>
+								</figcaption>
+							</li>
+						</a>
+							<a class="project-link">
+							<li><img src="assets/img/glitchr.jpg"></img>
+								<figcaption>
+									<h4>Project</h4>
+									<p>This is what my project does,guys!</p>
+								</figcaption>
+							</li>
+						</a>
+							<a class="project-link">
+							<li><img src="assets/img/glitchr.jpg"></img>
+								<figcaption>
+									<h4>Project</h4>
+									<p>This is what my project does,guys!</p>
+								</figcaption>
+							</li>
+						</a>
+							<a class="project-link">
+							<li><img src="assets/img/glitchr.jpg"></img>
+								<figcaption>
+									<h4>Project</h4>
+									<p>This is what my project does,guys!</p>
+								</figcaption>
+							</li>
+						</a>
+							<a class="project-link">
+							<li><img src="assets/img/glitchr.jpg"></img>
+								<figcaption>
+									<h4>Project</h4>
+									<p>This is what my project does,guys!</p>
+								</figcaption>
+							</li>
+						</a>
+							<a class="project-link">
+							<li><img src="assets/img/glitchr.jpg"></img>
+								<figcaption>
+									<h4>Project</h4>
+									<p>This is what my project does,guys!</p>
+								</figcaption>
+							</li>
+						</a>
+						</ul>
+					<div class="resume">
+						<h3>- RESUME -</h3>
+						<hr></hr>
+						<div class="resume-content">
+							<div class="left">
+								<!-- WORK EXPERIENCE -->
+
+								<h2>Work Experience</h2>
+								<div class="work">
+									<p class="position">Student Computer Technician</p>	
+									<p class="location">Ohio State University</p>
+									<p class="date">August 2012 - Present</p>
+									<p class="content">
+										<i class="fa fa-angle-right"></i></icon> Personally <span class="important">fixed over 150 university computers.</span><br/>
+										<i class="fa fa-angle-right"></i></icon> Taught faculty how to use education technologies such as SmartBoards.<br/>
+										<i class="fa fa-angle-right"></i></icon> <span class="important">Automated asset tracking </span> improving total time spent from a projected twenty hours to one.
+									</p>
+								</div>
+								<div class="work">	
+									<p class="position">Form Factor Reference Design Intern</p>
+									<p class="location">Intel Corporation</p>
+									<p class="date">Summer 2013</p>
+									
+									<p class="content">
+										<i class="fa fa-angle-right"></i></icon> Devised and implemented features for next-generation hardware in Windows 8 devices.<br/>
+										<i class="fa fa-angle-right"></i></icon> Designed the <span class="important">UX and marketing materials</span> for an upcoming consumer product.<br/>
+										<i class="fa fa-angle-right"></i></icon> Worked with <span class="important">wireless access points</span>, <span class="important">3D cameras</span>, <span class="important">NFC</span>, <span class="important">C#</span>, and <span class="important">Android-based Java.<br/></span>
+									</p>
+								</div>
+
+								<div class="work">	
+									<p class="position">Form Factor Reference Design Intern (2013)</p>
+									<p class="location">Intel Corporation</p>
+									<p class="date">Summer 2012</p>
+
+									<p class="content">Software Prototyping Intern</p>
+										<i class="fa fa-angle-right"></i></icon> Implemented a personal <span class="important">security system</span> for Android and Windows systems.<br/>
+										<i class="fa fa-angle-right"></i></icon> Worked with <span class="important">Bluetooth 4, accelerometers, GPS, C#,</span> and <span class="important">Android-based Java.<br/></span>
+									</p>
+								</div>
+								<div class="work">	
+									<p class="position">Cyber Security Intern</p>
+									<p class="location">Battelle Memorial Institute</p>
+									<p class="date">Summer 2011</p>
+									<p class="content">
+										<i class="fa fa-angle-right"></i></icon> Worked with <span class="important">reverse engineering</span> and <span class="important">penetration testing</span> techniques.<br/>
+										<i class="fa fa-angle-right"></i></icon> Co-wrote and co-designed <span class="important">multi-touch Windows applications </span> in <span class="important">Java.<br/></pan>
+									</p>
+								</div>
+								<div class="work">	
+									<p class="position">Networking Solutions Intern</p>
+									<p class="location">Cardinal Commerce</p>
+									<p class="date">Summer 2010</p>
+									<p class="content">
+										<i class="fa fa-angle-right"></i></icon> Helped <span class="important">create</span> and <span class="important">manage</span> the <span class="important">company's network</span>, focusing on security and online transactions.<br/>
+										<i class="fa fa-angle-right"></i></icon> Co-developed <span class="important">Java-based network utilities</span>.
+									</p>
+								</div>
+							</div>
+							<div class="right">
+								<!-- EDUCATION -->
+
+								<h2>Education</h2>
+								<div class="work">	
+									<p class="position">Bachelor of Sciences in Computer Information Sciences</p>
+									<p class="location">Ohio State University</p>
+									<p class="date">Expected May 2014</p>
+									<p class="content">
+										<icon class="icon-angle-right"></icon> Concentration GPA: 3.3/4.0.<br/>
+									</p>
+								</div>
+
+								<!-- PROJECTS AND AWARDS -->
+
+								<h2 class="new">Projects & Awards</h2>
+								<div class="work">	
+									<p class="position">Glitchr</p>
+									<p class="content">
+										<icon class="icon-angle-right"></icon> Wrote an <span class="important">image processing Android app</span> that allows users to add retro effects to their photos.<br/>
+										<icon class="icon-angle-right"></icon> Effects include Original Gameboy, Gameboy Camera, Glitch Art, and VHS tape.<br/>
+										<icon class="icon-angle-right"></icon> Currently <span class="important">available in the Google Play Store</span>.<br/>
+									</p>
+								</div>
+								<div class="work">	
+									<p class="position">Looking Glass</p>
+									<p class="location">Entry for hackMIT 2013</p>
+									<p class="details">
+										<icon class="icon-angle-right"></icon> Developed an app for <span class="important">Google Glass</span> that uses the camera, <span class="important">OCR</span>, and the speaker to assist the visually impaired. <br/>
+										<icon class="icon-angle-right"></icon> Using object recognition and OCR, the app told the user what they were holding.<br/>
+										<icon class="icon-angle-right"></icon> Examples include what cereal box they have or a pill bottle's instructions.
+									</p>
+								</div>
+
+								<div class="work">	
+									<p class="position">Reddit CAPTCHA Cracker</p>
+									<p class="content">
+										<icon class="icon-angle-right"></icon> Conducted independent research on <span class="important">machine learning</span> and <span class="important">image processing.</span><br/>
+										<icon class="icon-angle-right"></icon>  Successfully cracked Reddit's CAPTCHA system to demonstrate the inherent weakness of CAPTCHAs.<br/>
+									</p>
+								</div>
+								<div class="work">	
+									<p class="position">Gesture-Based Calculator</p>
+									<p class="location">1st Place in the Microsoft Winter App Hackathon 2013</p>
+									<p class="details">
+										<icon class="icon-angle-right"></icon> Designed and implemented a <span class="important">C#-based calculator</span> that used <span class="important">intuitive gestures</span>, such as "swipe left to subtract"" as its input.<br/>
+									</p>
+								</div>
+
+								<!-- SKILLS AND INTERESTS -->
+								<h2 class="new">Skills & Interests</h2>
+								<table>
+									<tr>
+										<td class="category"><icon class="icon-angle-right"></icon> Programming</td>
+										<td class="skills">Java, Android-Based Java, C#, C/C++</td>
+									</tr>
+									<tr>
+										<td class="category"><icon class="icon-angle-right"></icon> Hardware Experience </td>
+										<td class="skills">3D Camera's software, NFC, Bluetooth 4, Google Glass, Arduino, Raspberry Pi</td>
+									</tr>
+									<tr>
+										<td class="category"><icon class="icon-angle-right"></icon> Hobbies</td>
+										<td class="skills">Hiking, building furniture, traveling, watching anything with Tina Fey</td>
+									</tr>
+								</table>
+							</div>
+						</div>
+					</div>
+
+					<div class="footer">
+						<h3>- CONTACT -</h3>
+						<hr></hr>
+						<h4>You can contact me at <a href="mailto:me@bybrandonsmith.com">me@bybrandonsmith.com</a></h4>
+						<h4><i class="fa fa-repeat"></i> Reload for a<br/>new Glitchr effect!</h4>
+					</div>
+				</div>
+			</div>
+		</div>
+		<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+		<script type="text/javascript" src="assets/js/scripts.js"></script>
+	</body>
+</html>
